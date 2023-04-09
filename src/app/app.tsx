35491b2124fffc04f055885e40527afd7cc05fa5
Ethan <ethtaylan@gmail.com>
2023-04-09 08:43:25 +0200

    fastpush

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,14 +2,5 @@
 import './app.module.styl';
 
 export function App() {
-  return (
-    <>
-      <p className="display-1 text-center">Hello !</p>
-      <p className="text-center">
-        You are using Nx React Preset with TypeScript. Boostrap and Stylus for
-        CSS
-      </p>
-      <p className="text-center">Enjoy :)</p>
-    </>
-  );
+  return <h1>Hello world</h1>;
 }
